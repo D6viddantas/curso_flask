ca37d61ec87ce414b92fb8b1126f4328084c6c14
David Dantas <d6v1ddantas@gmail.com>
2025-05-04 21:56:47 -0300

    Utilizando variaveis no html(jinja)

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def principal():
-    return render_template('index.html')
+    conteudo1 = 'Manipulação de dados'
+    conteudo2 = 'Herança e Template'
+    conteudo3 = 'integração de API'
+    conteudo4 = 'Banco de Dados'
+    return render_template('index.html',
+                           conteudo1=conteudo1,
+                           conteudo2=conteudo2,
+                           conteudo3=conteudo3,
+                           conteudo4=conteudo4,
+                           )
 
 @app.route('/sobre')
 def sobre():
