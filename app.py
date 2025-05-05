5229e8b70e96853945852c7be1861039cc2053b4
David Dantas <d6v1ddantas@gmail.com>
2025-05-04 22:06:27 -0300

    Recuperando valores em uma lista,baiscamente usando um 'django template language'

--- app.py
+++ app.py
@@ -4,15 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def principal():
-    conteudo1 = 'Manipulação de dados'
-    conteudo2 = 'Herança e Template'
-    conteudo3 = 'integração de API'
-    conteudo4 = 'Banco de Dados'
+    conteudos = [
+        'Manipulação de dados',
+        'Herança e Template',
+        'integração de API',
+        'Banco de Dados']
     return render_template('index.html',
-                           conteudo1=conteudo1,
-                           conteudo2=conteudo2,
-                           conteudo3=conteudo3,
-                           conteudo4=conteudo4,
+                        conteudos=conteudos
                            )
 
 @app.route('/sobre')
